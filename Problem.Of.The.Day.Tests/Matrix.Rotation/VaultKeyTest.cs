cc777a5e6b05513c5a5fd0b32de8528c7703ddd2
Jakub Chodounsky <jakub.chodounsky@gmail.com>
2014-03-21 18:42:20 +0100

    Matrix problem solved, the dog is free!

---
+++ Problem.Of.The.Day.Tests/Matrix.Rotation/VaultKeyTest.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem.Of.The.Day.Matrix.Rotation;
+using System.Collections.Generic;
+
+namespace Problem.Of.The.Day.Tests.Matrix.Rotation
+{
+    [TestClass]
+    public class VaultKeyTest
+    {
+        private static IEnumerable<DayOfWeek> WeekDays = new[] { DayOfWeek.Monday, DayOfWeek.Tuesday, DayOfWeek.Wednesday, DayOfWeek.Thursday, DayOfWeek.Friday };
+        private static IEnumerable<DayOfWeek> WeekendDays = new[] { DayOfWeek.Saturday, DayOfWeek.Sunday };
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ThrowsForNullInput()
+        {
+            var key = new VaultKey();
+            key.GetCode(null, DayOfWeek.Monday);
+
+            var action = new Action<DayOfWeek>(day => key.GetCode(new int[][] { null }, day));
+            
+            AssertFor(action, WeekDays);
+            AssertFor(action, WeekendDays);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThrowsForEmptyInput()
+        {
+            var key = new VaultKey();
+            
+            var action = new Action<DayOfWeek>(day => key.GetCode(new int[0][], day));
+
+            AssertFor(action, WeekDays);
+            AssertFor(action, WeekendDays);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void ThrowsForInvalidInput()
+        {
+            var input = new int[][] { new[] { 1, 2 }, new[] { 2 } };
+            var key = new VaultKey();
+
+            var action = new Action<DayOfWeek>(day => key.GetCode(input, day));
+
+            AssertFor(action, WeekDays);
+            AssertFor(action, WeekendDays);
+        
+        }
+
+        [TestMethod]
+        public void ReturnsForSingleValue()
+        {
+            var input = new int[][] { new[] { 1 } };
+            var key = new VaultKey();
+
+            var action = new Action<DayOfWeek>(day =>
+                {
+                    var result = key.GetCode(input, day);
+                    AreEquivalent(result, input);
+                });
+
+            AssertFor(action, WeekDays);
+            AssertFor(action, WeekendDays);
+        }
+
+        [TestMethod]
+        public void ReturnsForWeekDays()
+        {
+            var input = new int[][] { new[] { 1, 2 }
+                                    , new[] { 3, 4 } };
+            var key = new VaultKey();
+
+            var action = new Action<DayOfWeek>(day =>
+            {
+                var result = key.GetCode(input, day);
+                Assert.IsTrue(AreEquivalent(result, new int[][]{ new[] { 3, 1 }
+                                                               , new[] { 4, 2 }}));
+                
+            });
+
+            AssertFor(action, WeekDays);
+        }
+
+        [TestMethod]
+        public void ReturnsForWeekendDays()
+        {
+            var input = new int[][] { new[] { 1, 2 }
+                                    , new[] { 3, 4 } };
+            var key = new VaultKey();
+
+            var action = new Action<DayOfWeek>(day =>
+            {
+                var result = key.GetCode(input, day);
+                Assert.IsTrue(AreEquivalent(result, new int[][]{ new[] { 2, 4 }
+                                                               , new[] { 1, 3 }}));
+            });
+
+            AssertFor(action, WeekendDays);
+        }
+
+        [TestMethod]
+        public void DoesNotModifyInput()
+        {
+            var input = new int[][] { new[] { 1, 2 }
+                                    , new[] { 3, 4 } };
+            var key = new VaultKey();
+
+            var action = new Action<DayOfWeek>(day =>
+            {
+                var result = key.GetCode(input, day);
+                Assert.IsFalse(AreEquivalent(input, result));
+            });
+
+            AssertFor(action, WeekDays);
+            AssertFor(action, WeekendDays);
+        }
+        
+
+        private static void AssertFor(Action<DayOfWeek> action, IEnumerable<DayOfWeek> days)
+        {
+            foreach (var day in days)
+            {
+                action(day);
+            }
+        }
+
+        private static bool AreEquivalent(int[][] first, int[][] second)
+        {
+            int n = first.Length;
+
+            for (int row = 0; row < n; row++)
+            {
+                if (n != second[row].Length)
+                    return false;
+
+                for (int column = 0; column < n; column++)
+                {
+                    if (first[row][column] != second[row][column])
+                        return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
+
---
+++ Problem.Of.The.Day/Matrix.Rotation/VaultKey.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem.Of.The.Day.Matrix.Rotation
+{
+    /// <summary>
+    /// Matrix Rotation
+    /// The bank manager at my local bank recently gave me the algorithm to access the bank's vault. I thought I'd pass on the algorithm to you all for "safe keeping". Basically the vault has a USB port which you'll need to plug in to. Once plugged in the vault will send you an NxN matrix such as the one below.
+    /// Monday-Friday the key to the vault is to rotate the matrix 90 degrees clockwise. On Saturday and Sunday you have to rotate the matrix 90 degrees counter-clockwise. My dog accidentally got locked in the vault and the bank manager is no where to be found. Can someone help me write a program to get him out?
+    /// </summary>
+    public sealed class VaultKey
+    {
+        public int[][] GetCode(int[][] input, DayOfWeek day)
+        {
+            if (input == null)
+                throw new ArgumentNullException("input");
+            if (input.Any(i => i == null))
+                throw new ArgumentNullException("input", "Can't pass an input with null row");
+            if (input.Length == 0)
+                throw new ArgumentOutOfRangeException("input", "Can't work with an empty array");
+            if (!IsSquare(input))
+                throw new ArgumentOutOfRangeException("input", "Input must be of N x N");
+
+            var code = (int[][])input.Clone();
+            if (code.Length == 1)
+                return code;
+
+            return day == DayOfWeek.Saturday || day == DayOfWeek.Sunday ?
+                TransformCounterClockwise(input) : TransformClockwise(input);
+        }
+
+        private bool IsSquare(int[][] input)
+        {
+            return input.All(row => row.Length == input.Length);
+        }
+
+        private int[][] TransformClockwise(int[][] input)
+        {
+            int n = input.Length;
+            var code = new int[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                code[row] = new int[n];
+
+                for (int column = 0; column < n; column++)
+                {
+                    code[row][column] = input[n - 1 - column][row];
+                }
+            }
+
+            return code;
+        }
+
+        private int[][] TransformCounterClockwise(int[][] input)
+        {
+            int n = input.Length;
+            var code = new int[n][];
+
+            for (int row = 0; row < n; row++)
+            {
+                code[row] = new int[n];
+                
+                for (int column = 0; column < n; column++)
+                {
+                    code[row][column] = input[column][n - 1 - row];
+                }
+            }
+
+            return code;
+        }
+    }
+}
